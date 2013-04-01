6f88eaa2bc1fb66f3ad6c9ace66b4babb2e0a9be
Dave Eddy <dave@daveeddy.com>
2013-03-31 19:25:15 -0700

    dos2unix and indentation

--- lib/siriproxy-externalip.rb
+++ lib/siriproxy-externalip.rb
@@ -4,10 +4,10 @@ require 'net/http'
 
 class SiriProxy::Plugin::Externalip < SiriProxy::Plugin
   listen_for /external ip address/i do |state|
-		url = URI.parse('http://ifconfig.me/ip')
-		ip = Net::HTTP.get(url)
-		humanreadableip = ip.gsub('', ' ').gsub('.', 'dot')
-		say "You're external IP is #{ip}", spoken: "You're external IP is #{humanreadableip}"
+    url = URI.parse('http://ifconfig.me/ip')
+    ip = Net::HTTP.get(url)
+    humanreadableip = ip.gsub('', ' ').gsub('.', 'dot')
+    say "You're external IP is #{ip}", spoken: "You're external IP is #{humanreadableip}"
     request_completed
   end
 end
